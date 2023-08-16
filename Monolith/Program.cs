5eb7ddd999d43d78984a93e3ae6c4144c3bdc89e
Dieter <Nunnenmacher>
2023-08-16 11:37:24 +0200

    Add project files.

---
+++ Monolith/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Monolith
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (!File.Exists(args[0]))
+            {
+                await File.Create(args[0]).DisposeAsync();
+            }
+            var userCounts = (await File.ReadAllLinesAsync(args[0])).ToDictionary(line => line.Split(' ')[0], line => int.Parse(line.Split(' ')[1]));
+            Console.WriteLine("Please enter your name:");
+            var name = Console.ReadLine() ?? string.Empty;
+            if (!userCounts.ContainsKey(name))
+            {
+                userCounts[name] = 1;
+                Console.WriteLine($"Hello, {name}!");
+            }
+            else
+            {
+                userCounts[name]++;
+                int count = userCounts[name];
+                if (count <= 3)
+                {
+                    Console.WriteLine($"Welcome back, {name}!");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello, my good friend {name}!");
+                }
+            }
+            await File.WriteAllLinesAsync(args[0], userCounts.Select(entry => $"{entry.Key} {entry.Value}"));
+        }
+    }
+}
